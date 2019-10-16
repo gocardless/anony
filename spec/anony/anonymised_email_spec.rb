8b1b3aa1354688b85cefb5d4992ebd0a3185d2f0
Michael Wawra <michaelwawra@gocardless.com>
2019-10-16 11:00:15 +0100

    Fix tests for anonymiser for email
    - We should test against a known value to make sure that we're assert
    the anonymiser strategy is doing the right thing...
    - But conversely I don't want this test to cause issues with other
    things that are expecting the config (which is in code...)
    - Nor do we want to test against the config as it makes the test brittle
    (slightly contradictory to the above...)
    
    This feels right...

--- spec/anony/anonymised_email_spec.rb
+++ spec/anony/anonymised_email_spec.rb
@@ -6,8 +6,17 @@ RSpec.describe Anony::AnonymisedEmail do
   describe ".call" do
     subject(:result) { described_class.call(value) }
 
+    around do |example|
+      original = Anony::Config.email_template
+      Anony::Config.email_template = "anonymous+%s@test-domain.com"
+
+      example.run
+
+      Anony::Config.email_template = original
+    end
+
     let(:value) { "old value" }
 
-    it { is_expected.to match(/^anonymous\+[0-9a-f\-]+@example.com$/) }
+    it { is_expected.to match(/^anonymous\+[0-9a-f\-]+@test-domain.com$/) }
   end
 end
