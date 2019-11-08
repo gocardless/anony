b3d68fe5afa4a87d943335ab5c5605c91917df61
Nick Campbell <nickcampbell@gocardless.com>
2019-11-08 15:31:10 +0000

    Fix relative paths following branch filtering

--- lib/anony.rb
+++ lib/anony.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Anony
-  require_relative "anonymisable"
-  require_relative "anonymised_email"
-  require_relative "anonymised_phone_number"
-  require_relative "config"
-  require_relative "field_exception"
-  require_relative "nilable"
-  require_relative "no_op"
-  require_relative "overwrite_hex"
-  require_relative "strategy_exception"
+  require_relative "anony/anonymisable"
+  require_relative "anony/anonymised_email"
+  require_relative "anony/anonymised_phone_number"
+  require_relative "anony/config"
+  require_relative "anony/field_exception"
+  require_relative "anony/nilable"
+  require_relative "anony/no_op"
+  require_relative "anony/overwrite_hex"
+  require_relative "anony/strategy_exception"
 end
