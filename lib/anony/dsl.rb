865ae2c4152907b96b3a5db50e5809bf01cf5f25
Nick Campbell <nickcampbell@gocardless.com>
2019-11-25 20:57:09 +0000

    Document Anony::Anonymisable, DSL and & Config

--- lib/anony/dsl.rb
+++ lib/anony/dsl.rb
@@ -1,14 +1,50 @@
 # frozen_string_literal: true
 
 module Anony
+  # The interface for configuring strategies. All of the methods here are made available
+  # inside the `anonymise { ... }` block:
+  #
+  # @example
+  #   anonymise do
+  #     nilable :first_name
+  #     email :email_address
+  #     with_strategy(:last_name) { "last-#{id}" }
+  #   end
   class DSL
+    # @!visibility private
     def initialize
       @anonymisable_fields = {}
       @destroy_on_anonymise = false
     end
 
+    # @!visibility private
     attr_reader :anonymisable_fields, :destroy_on_anonymise
 
+    # Configure a custom strategy for one or more fields. If a block is given that is used
+    # as the strategy, otherwise the first argument is used as the strategy.
+    #
+    # @param [Proc, Object] strategy Any object which responds to `.call(previous_value)`. Not used
+    #   if a block is provided.
+    # @param [Array<Symbol>] fields A list of one or more fields to apply this strategy to.
+    # @param [Block] &block A block to use as the strategy.
+    # @yieldparam previous [Object] The previous value of the field
+    # @yieldreturn [Object] The value to set on that field.
+    # @raise [ArgumentError] If the combination of strategy, fields and block is invalid.
+    #
+    # @example With a named class
+    #   class Reverse
+    #     def self.call(previous)
+    #       previous.reverse
+    #     end
+    #   end
+    #
+    #   with_strategy(Reverse, :first_name)
+    #
+    # @example With a constant value
+    #   with_strategy({}, :metadata)
+    #
+    # @example With a block
+    #   with_strategy(:first_name, :last_name) { |previous| previous.reverse }
     def with_strategy(strategy, *fields, &block)
       if block_given?
         fields.unshift(strategy)
@@ -24,26 +60,51 @@ module Anony
       fields.each { |field| anonymisable_fields[field] = strategy }
     end
 
+    # Helper method to use the :hex strategy
+    # @param [Array<Symbol>] fields A list of one or more fields to apply this strategy to.
+    # @see Strategies::OverwriteHex
     def hex(*fields, max_length: 36)
       with_strategy(Strategies::OverwriteHex.new(max_length), *fields)
     end
 
+    # Helper method to use the :email strategy
+    # @param [Array<Symbol>] fields A list of one or more fields to apply this strategy to.
+    # @see Strategies::AnonymisedEmail
     def email(*fields)
       with_strategy(Strategies::AnonymisedEmail, *fields)
     end
 
+    # Helper method to use the :phone_number strategy
+    # @param [Array<Symbol>] fields A list of one or more fields to apply this strategy to.
+    # @see Strategies::AnonymisedPhoneNumber
     def phone_number(*fields)
       with_strategy(Strategies::AnonymisedPhoneNumber, *fields)
     end
 
+    # Helper method to use the :nilable strategy
+    # @param [Array<Symbol>] fields A list of one or more fields to apply this strategy to.
+    # @see Strategies::Nilable
     def nilable(*fields)
       with_strategy(Strategies::Nilable, *fields)
     end
 
+    # Helper method to use the :current_datetime strategy
+    # @param [Array<Symbol>] fields A list of one or more fields to apply this strategy to.
+    # @see Strategies::CurrentDatetime
     def current_datetime(*fields)
       with_strategy(Strategies::CurrentDatetime, *fields)
     end
 
+    # Configure a list of fields that you don't want to anonymise.
+    #
+    # @param [Array<Symbol>] fields The fields to ignore
+    # @raise [ArgumentError] If trying to ignore a field which is already globally
+    #   ignored in Anony::Config.ignores
+    #
+    # @example
+    #   anonymise do
+    #     ignore :external_system_id, :externalised_at
+    #   end
     def ignore(*fields)
       already_ignored = fields.select { |field| Config.ignore?(field) }
 
@@ -55,6 +116,15 @@ module Anony
       with_strategy(Strategies::NoOp, *fields)
     end
 
+    # Use the deletion strategy instead of anonymising individual fields. This method is
+    # incompatible with the other methods in this file.
+    #
+    # @raise [ArgumentError] If other strategies are used in the same class
+    #
+    # @example
+    #   anonymise do
+    #     destroy
+    #   end
     def destroy
       unless anonymisable_fields.empty?
         raise ArgumentError, "Can't specify destroy and strategies for fields"
