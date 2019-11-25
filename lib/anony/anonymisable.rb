ab3330a0c76df8ab1749f0b3b33f65a0a0e38658
Nick Campbell <nickcampbell@gocardless.com>
2019-11-25 20:57:09 +0000

    Document Anony::Anonymisable, DSL and & Config

--- lib/anony/anonymisable.rb
+++ lib/anony/anonymisable.rb
@@ -5,12 +5,59 @@ require "active_support/core_ext/module/delegation"
 require_relative "dsl"
 
 module Anony
+  # The main Anony object to include in your ActiveRecord class.
+  #
+  # @example Using in a single model
+  #   class Manager < ApplicationRecord
+  #     include Anony::Anonymisable
+  #   end
+  #
+  # @example Making this available to your whole application
+  #   class ApplicationRecord < ActiveRecord::Base
+  #     include Anony::Anonymisable
+  #   end
   module Anonymisable
     ANONYMISED_AT = :anonymised_at
+    private_constant :ANONYMISED_AT
 
+    # Mixin containing methods that will be exposed on the ActiveRecord class after
+    # including the Anonymisable module.
+    #
+    # The primary method, #anonymise, is used to configure the strategies to apply. This
+    # configuration is lazily executed when trying to actually anonymise an instance:
+    # this is because the database or other lazily-loaded properties are not necessarily
+    # available when the class is configured.
     module ClassMethods
+      # Define a set of anonymisation configuration on the ActiveRecord class.
+      #
+      # @yield A configuration block
+      # @see DSL Anony::DSL - the methods available inside this block
+      # @example
+      #   class Manager < ApplicationRecord
+      #     anonymise do
+      #       with_strategy(:first_name) { "ANONYMISED" }
+      #     end
+      #   end
       def anonymise(&block)
         anonymise_config.instance_eval(&block)
+      end
+
+      # Check whether the model has been configured correctly. Returns a simple true/false.
+      #
+      # @return [Boolean]
+      # @example
+      #   Manager.valid_anonymisation?
+      def valid_anonymisation?
+        destroy_on_anonymise || unhandled_fields.empty?
+      end
+
+      # Validates the configuration and raises an exception if it is invalid.
+      # @raise [FieldException]
+      # @return [nil]
+      # @example
+      #   Manager.validate_anonymisation!
+      def validate_anonymisation!
+        raise FieldException, unhandled_fields unless valid_anonymisation?
       end
 
       private def anonymise_config
@@ -18,14 +65,6 @@ module Anony
       end
 
       delegate :anonymisable_fields, :destroy_on_anonymise, to: :anonymise_config
-
-      def valid_anonymisation?
-        destroy_on_anonymise || unhandled_fields.empty?
-      end
-
-      def validate_anonymisation!
-        raise FieldException, unhandled_fields unless valid_anonymisation?
-      end
 
       private def unhandled_fields
         anonymisable_columns =
@@ -39,6 +78,16 @@ module Anony
       end
     end
 
+    # Run all anonymisation strategies on the model instance before saving it.
+    #
+    # @return [true] if the save was successful
+    # @raise [FieldException] if the configuration is invalid (configuration is lazily validated)
+    # @raise [ActiveRecord::RecordNotSaved] if the save failed (e.g. a validation error)
+    # @raise [ActiveRecord::RecordNotDestroyed] if the destroy failed (where this strategy
+    #   has been configured)
+    # @example
+    #   manager = Manager.first
+    #   manager.anonymise!
     def anonymise!
       self.class.validate_anonymisation!
 
@@ -78,6 +127,7 @@ module Anony
       end
     end
 
+    # @!visibility private
     def self.included(base)
       base.extend(ClassMethods)
     end
--- lib/anony/config.rb
+++ lib/anony/config.rb
@@ -3,11 +3,40 @@
 require "active_support/core_ext/module/attribute_accessors"
 
 module Anony
+  # Configuration which modifies how the gem will behave in your application. It's
+  # recommended practice to configure this in an initializer.
+  #
+  # @example
+  #   # config/initializers/anony.rb
+  #   require "anony"
+  #
+  #   Anony::Config.ignore_fields(:id)
   module Config
     mattr_accessor :ignores
+
+    # @!attribute email_template
+    #   @!scope class
+    #   The email template that will be used when using the :email strategy. If you
+    #   optionally define a `%s` inside a string, it will be replaced with
+    #   `SecureRandom.uuid`.
+    #   @see Strategies::AnonymisedEmail
+    #
+    #   @example With string substitution
+    #     Anony::Config.email_template = "anonymised-%s@example.com"
+    #
+    #   @example Static string
+    #     Anony::Config.email_template = "nobody@example.net"
     mattr_accessor :email_template
+
+    # @!attribute phone_number
+    #   @!scope class
+    #   The phone number that will be used when using the :phone_number strategy.
+    #   @see Strategies::AnonymisedPhoneNumber
+    #   @example
+    #     Anony::Config.phone_number = "+44 1632 960670"
     mattr_accessor :phone_number
 
+    # @!visibility private
     def self.ignore?(field)
       # In this case, we want to support literal matches, regular expressions and blocks,
       # all of which are helpfully handled by Object#===.
@@ -17,6 +46,15 @@ module Anony
       # rubocop:enable Style/CaseEquality
     end
 
+    # A list of database or model properties to be ignored when anonymising. This is
+    # helpful in Rails applications when there are common columns such as `id`,
+    # `created_at` and `updated_at`, which we would never want to try and anonymise.
+    #
+    # By default, this is an empty collection (i.e. no fields are ignored).
+    #
+    # @param [Array<Symbol>] fields A list of fields names to ignore.
+    # @example Ignoring common Rails fields
+    #   Anony::Config.ignore_fields(:id, :created_at, :updated_at)
     def self.ignore_fields(*fields)
       self.ignores = Array(fields)
     end
